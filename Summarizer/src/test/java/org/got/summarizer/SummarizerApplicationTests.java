f808644bbd7cd0a59c81b420c1cd7a0a33c62bdc
Michael Lechowicz <ichal.lechowicz@gmail.com>
2023-05-03 21:20:21 +0200

    Add module Summarizer

---
+++ Summarizer/src/test/java/org/got/summarizer/SummarizerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.got.summarizer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SummarizerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
