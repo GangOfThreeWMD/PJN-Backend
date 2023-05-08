dca6a5285dcac7f83cca23edc14ed90071cbe538
Wojciech Najdek <najdekwojtek1@gmail.com>
2023-05-08 19:44:29 +0200

    Fix typos

--- PythonModule/summarizer.py
+++ PythonModule/summarizer.py
@@ -55,7 +55,7 @@ def build_similarity_matrix(sentences, stop_words):
 
     for idx1 in range(len(sentences)):
         for idx2 in range(len(sentences)):
-            if idx1 == idx2: #ignore if both are same sentences
+            if idx1 == idx2:  # ignore if both are same sentences
                 continue
             similarity_matrix[idx1][idx2] = sentence_similarity(sentences[idx1], sentences[idx2], stop_words)
 
@@ -66,18 +66,18 @@ def generate_summary(text, top_n=5, is_file_path=False, min_length=128):
     stop_words = stopwords.words('english')
     summarize_text = []
 
-    # Step 1 - Read text anc split it
+    # Step 1 - Read text and split it
     sentences = read_article(text, is_file_path)
 
-    # Step 2 - Generate Similary Martix across sentences
-    sentence_similarity_martix = build_similarity_matrix(sentences, stop_words)
+    # Step 2 - Generate Similarity Matrix across sentences
+    sentence_similarity_matrix = build_similarity_matrix(sentences, stop_words)
 
-    # Step 3 - Rank sentences in similarity martix
-    sentence_similarity_graph = nx.from_numpy_array(sentence_similarity_martix)
+    # Step 3 - Rank sentences in similarity matrix
+    sentence_similarity_graph = nx.from_numpy_array(sentence_similarity_matrix)
     scores = nx.pagerank(sentence_similarity_graph)
 
     # Step 4 - Sort the rank and pick top sentences
-    ranked_sentence = sorted(((scores[i],s) for i,s in enumerate(sentences)), reverse=True)
+    ranked_sentence = sorted(((scores[i], s) for i, s in enumerate(sentences)), reverse=True)
     # print("Indexes of top ranked_sentence order are ", ranked_sentence)
 
     if len(ranked_sentence) < top_n:
@@ -89,7 +89,7 @@ def generate_summary(text, top_n=5, is_file_path=False, min_length=128):
         summarize_text.append(" ".join(ranked_sentence[i][1]))
         summarize_text.append(". ")
 
-    # Step 5 - Offcourse, output the summarize text
+    # Step 5 - output the summarize text
     return "".join(summarize_text)
 
 
@@ -102,4 +102,3 @@ if __name__ == '__main__':
     init()
     output = generate_summary("msft.txt", 2, True)
     print(output)
-
